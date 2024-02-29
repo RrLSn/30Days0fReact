196c1f4922f9623a026aa6add89bc1dfc3221958
RrLSn <afolabisodiqtemitope@yahoo.com>
2024-02-29 13:55:36 +0100

    Day1

--- DAY1_Js refresher/day1.js
+++ DAY1_Js refresher/day1.js
@@ -1,4 +1,4 @@
-// Exercise: Level 1
+// Exercise1: Level 1
 const countries = [
   "Albania",
   "Bolivia",
@@ -184,7 +184,7 @@ console.log(removeLastIT);
 const removeAllIT = itCompanies.splice();
 console.log(removeAllIT);
 
-// Exercise 2
+// Level 2
 // 1. Create a separate countries.js file and store the countries array into this file, create a separate file web_techs.js and store the webTechs array into this file. Access both file in main.js file
 
 const web_techs = webTechs;
@@ -239,14 +239,252 @@ const backEnd = ["Node", "Express", "MongoDB"];
 const fullStack = frontEnd.concat(backEnd);
 console.log(fullStack);
 
-// Exercise 3
+// Level 3
 // 1. The following is an array of 10 students ages: js const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]  - Sort the array and find the min and max age - Find the median age(one middle item or two middle items divided by two) - Find the average age(all items divided by number of items) - Find the range of the ages(max minus min) - Compare the value of (min - average) and (max - average), use abs() method
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+const sortedAges = ages.sort((a, b) => a - b);
+sortedAges;
+const minAge = Math.min(...sortedAges);
+const maxAges = Math.max(...sortedAges);
+sortedAges;
+const range = maxAges - minAge;
+let medianAge;
+const middleIndex = Math.floor(sortedAges.length / 2);
+middleIndex;
+if (sortedAges.length % 2 === 0) {
+  medianAge = (sortedAges[middleIndex - 1] + sortedAges[middleIndex]) / 2;
+} else {
+  medianAge = sortedAges[middleIndex];
+}
+let sum = 0;
+sortedAges;
+for (let i = 0; i < sortedAges.length; i++) {
+  sum += sortedAges[i];
+}
+console.log(sum);
+
+const minAverageValue = Math.abs(minAge - range);
+const maxAveargeValue = Math.abs(maxAges - range);
 
 // 1.Slice the first ten countries from the countries array
 const slicedCountries = countries.slice(0, 10).join(", ");
 console.log(slicedCountries);
 
 // 2. Find the middle country(ies) in the countries array
+let midCountry;
+const midCountryIndex = Math.floor(countriesName.length / 2);
+if (countriesName.length % 2 === 0) {
+  midCountry =
+    countriesName[midCountryIndex - 1] + countriesName[midCountryIndex] / 2;
+} else {
+  midCountry = countriesName[midCountryIndex];
+}
+
+console.log(midCountry);
 
 // 3. Divide the countries array into two equal arrays if it is even. If countries array is not even , one more country for the first half.
+let firstHalf;
+let secondHalf;
+if (countriesName.length % 2 === 0) {
+  firstHalf = countriesName.slice(0, midCountryIndex);
+  secondHalf = countriesName.splice(midCountryIndex + 1);
+} else {
+  countriesName.push("Nigeria");
+  firstHalf = countriesName.slice(0, midCountryIndex + 1);
+  secondHalf = countriesName.splice(midCountryIndex + 1);
+}
+console.log(firstHalf);
+console.log(secondHalf);
+
+// Exercise 2: Level 1
+// 1. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+
+// Enter your age: 30
+// You are old enough to drive.
+
+// Enter your age:15
+// You are left with 3 years to drive.
+
+/*let userAge = prompt("Enter your age");
+const drivingAge = 18;
+
+if (userAge >= drivingAge) {
+  console.log("You are old enough to drive.");
+} else {
+  const yearsToDriving = drivingAge - userAge;
+  console.log(`You are left with ${yearsToDriving} years to drive`);
+}*/
+
+// 2. Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+
+// Enter your age: 30
+// You are 5 years older than me.
+
+/*const yourAge = prompt("Enter your age");
+const myAge = 25;
+
+if (yourAge > myAge) {
+  let agedifference = yourAge - myAge;
+  console.log(`You are ${agedifference} years older than me`);
+} else {
+  let agedifference = myAge - yourAge;
+  console.log(`I am ${agedifference} years older than you`);
+}*/
+
+// 3. If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in two ways
+// using if else
+// ternary operator.
+
+let a = 4;
+let b = 3;
+// 4 is greater than 3
+
+if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+} else {
+  console.log(`${b} is greater than ${a}`);
+}
+
+a > b
+  ? console.log(`${a} is greater than ${b}`)
+  : console.log(`${b} is greater than ${a}`);
+
+// 4. Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
+
+// Enter a number: 2
+// 2 is an even number
+
+// Enter a number: 9
+// 9 is is an odd number.
+
+/*const checkEvenNum = prompt("Enter a number");
+
+if (checkEvenNum % 2 === 0) {
+  console.log(`${checkEvenNum} is an even number`);
+} else {
+  console.log(`${checkEvenNum} is an odd number`);
+}*/
+
+// Level 2
+// 1. Write a code which can give grades to students according to theirs scores:
+// 80-100, A
+// 70-89, B
+// 60-69, C
+// 50-59, D
+// 0-49, F
+
+/*const grade = prompt("Enter your grade");
+switch (true) {
+  case grade >= 80 && grade <= 100:
+    console.log("Your Grade is A");
+    break;
+  case grade >= 70 && grade <= 89:
+    console.log("Your Grade is B");
+    break;
+  case grade >= 60 && grade <= 69:
+    console.log("Your Grade is C");
+    break;
+  case grade >= 50 && grade <= 59:
+    console.log("Your Grade is D");
+    break;
+  default:
+    console.log("Your Grade is F");
+}*/
+
+// 2. Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+// September, October or November, the season is Autumn.
+// December, January or February, the season is Winter.
+// March, April or May, the season is Spring
+// June, July or August, the season is Summer
+
+/*const enterSeason = prompt("Enter season");
+const season = enterSeason.toLowerCase();
+
+switch (season) {
+  case "september" || "october" || "november":
+    console.log("The season is Autumn");
+    break;
+  case "december" || "january" || "february":
+    console.log("The season is Winter");
+    break;
+  case "march" || "april" || "may":
+    console.log("The season is Spring");
+    break;
+  case "june" || "july" || "august":
+    console.log("The season is Summer");
+    break;
+  default:
+    console.log("Enter a valid season");
+}*/
+
+// 3. Check if a day is weekend day or a working day. Your script will take day as an input.
+// What is the day  today? Saturday
+// Saturday is a weekend.
+
+// What is the day today? saturDaY
+// Saturday is a weekend.
+
+// What is the day today? Friday
+// Friday is a working day.
+
+// What is the day today? FrIDAy
+// Friday is a working day.
+
+/*const checkForDay = prompt("What day is today?");
+const inputDay = checkForDay.toLowerCase();
+
+switch (inputDay) {
+  case "saturday" || "sunday":
+    console.log(`${inputDay.charAt(0).toUpperCase() + alphabet.slice(1)} is a weekend.`);
+    break;
+  case "monday" || "tuesday" || "wednesday" || "thursday" || "friday":
+    console.log(`${inputDay.charAt(0).toUpperCase() + alphabet.slice(1)} is a working day.`);
+    break;
+  default:
+    console.log("Enter a valid day.");
+}*/
+
+// Level 3
+// 1. Write a program which tells the number of days in a month.
+// Enter a month: January
+// January has 31 days.
+
+// Enter a month: JANUARY
+// January has 31 day
+
+// Enter a month: February
+// February has 28 days.
+
+// Enter a month: FEbruary
+// February has 28 days.
+
+/*const enterMonth = prompt("Enter a month");
+const inputMonth = enterMonth.toLowerCase();
+
+switch (inputMonth) {
+  case "february":
+    console.log(
+      `${inputMonth.charAt(0).toUpperCase() + alphabet.slice(1)} has 28 days`
+    );
+    break;
+  case "april" || "june" || "september" || "November":
+    console.log(
+      `${inputMonth.charAt(0).toUpperCase() + inputMonth.slice(0)} has 30 days`
+    );
+    break;
+  case "January" ||
+    "March" ||
+    "May" ||
+    "July" ||
+    "August" ||
+    "October" ||
+    "December":
+    console.log(
+      `${inputMonth.charAt(0).toUpperCase + inputMonth.slice(0)} has 31 days`
+    );
+    break;
+  default:
+    console.log("Invalid Month!");
+}*/
+
+// 2. Write a program which tells the number of days in a month, now consider leap year.
